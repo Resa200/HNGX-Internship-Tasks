146866e8884480ab7c65e146136b22e00bb34d65
Resa200 <180401046@live.unilag.edu.ng>
2023-09-08 14:04:07 +0100

    Task 1

--- TASK1/api.py
+++ TASK1/api.py
@@ -3,20 +3,11 @@ import datetime
 
 app = Flask(__name__)
 
-# Define a dictionary to store track names based on track codes
-tracks = {
-    "backend": "Backend",  # You can add more tracks as needed
-}
-
 @app.route('/api', methods=['GET'])
 def get_info():
     # Get query parameters slack_name and track
     slack_name = request.args.get('slack_name')
     track_code = request.args.get('track')
-
-    # Check if the track_code is valid
-    if track_code not in tracks:
-        return jsonify({"error": "Invalid track"}), 400
 
     # Get the current UTC time
     utc_time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -26,7 +17,7 @@ def get_info():
         "slack_name": slack_name,
         "current_day": datetime.datetime.now().strftime("%A"),
         "utc_time": utc_time,
-        "track": tracks[track_code],
+        "track": track_code,
         "github_file_url": "https://github.com/Resa200/HNGX-Internship-Tasks/blob/master/TASK1/api.py",
         "github_repo_url": "https://github.com/Resa200/HNGX-Internship-Tasks",
         "status_code": 200
